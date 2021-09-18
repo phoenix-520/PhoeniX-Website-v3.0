11eb43f1e0cdac227416faff9c1c0ed4055c27fc
Sidzi <ayturgankgs@gmail.com>
2021-09-18 19:47:54 +0600

    added project

--- js/app.js
+++ js/app.js
@@ -53,3 +53,24 @@ function changeSlide(deriction) {
     sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 
 }
+
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+    }
+
+    function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+    }
+}
+
+slidesPlugin()
